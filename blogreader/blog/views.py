298831a8dbc11cb2f713899a951676bf365b2940
Vedant Lahane <vedantanillahane@gmail.com>
2025-11-27 03:55:13 +0530

    Refactor code structure for improved readability and maintainability

--- blogreader/blog/views.py
+++ blogreader/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, "post_list.html", {"posts": posts})
