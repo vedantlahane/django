75079ca0d79bd8bf6f46876ea65e6b9e2f0b393e
Vedant Lahane <vedantanillahane@gmail.com>
2025-11-26 17:33:25 +0530

    Remove unused Django app files and configurations for profiles, blog, and portfolio applications, including views, URLs, models, and templates. Clean up project structure by deleting unnecessary files and directories.

--- myproject/profiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =[
-    path('profile/<str:username>/', views.profile),
-    path('users/', views.user_list, name='user_list'),
-    path('about/', views.about, name='about'),
-]
